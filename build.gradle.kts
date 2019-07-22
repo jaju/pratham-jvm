5d96735b4ac779caeeb484e55fe7f27f928a5efb
Ravindra R. Jaju <jaju@msync.org>
2019-07-22 14:04:09 +0530

    Repo with QueueTest

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+}
+
+group = "com.tavisca.workshops"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.json:json:20180813")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.0")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_12
+    targetCompatibility = JavaVersion.VERSION_12
+}
+
+tasks.register<Test>("hidden-tests")
+
+tasks.named<Test>("test") {
+    dependsOn("cleanTest")
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+        excludeTags("hidden")
+    }
+
+    testLogging {
+        showExceptions = true
+        events("passed", "skipped", "failed")
+    }
+}
+
+
+tasks.named<Test>("hidden-tests") {
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+        includeTags("hidden")
+    }
+
+    testLogging {
+        showExceptions = true
+        events("passed", "skipped", "failed")
+    }
+}
---
+++ src/test/java/com/tavisca/workshops/pratham/datastructures/QueueTest.java
@@ -0,0 +1,112 @@
+package com.tavisca.workshops.pratham.datastructures;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    private int capacity = 8;
+    private Queue<Integer> b;
+
+    @BeforeEach
+    void initialize() {
+        this.b = new Queue<>(this.capacity);
+    }
+
+    @Test
+    void isConstructedWithDefaultCapacity() {
+        assertEquals(Queue.defaultCapacity, new Queue().capacity());
+    }
+
+    @Test
+    void canBeConstructedWithSuppliedCapacity() {
+        int capacity = 64;
+        assertEquals(capacity, new Queue(capacity).capacity());
+    }
+
+    @Test
+    void acceptsAValueToAdd() {
+        b.add(10);
+        assertEquals(10, b.remove());
+    }
+
+    @Test
+    void acceptsMultipleValuesToAdd() {
+        for (int v : new int[]{1, 2, 3}) {
+            b.add(v);
+        }
+        assertEquals(1, b.remove());
+    }
+
+    @Test
+    void removesValuesInTheSameOrderAsAdded() {
+        int[] insertedValues = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        for (int v: insertedValues) {
+            b.add(v);
+        }
+        for (int v: insertedValues) {
+            assertEquals(b.remove(), v);
+        }
+    }
+
+    @Test
+    void throwsExceptionWhenCapacityIsExhausted() {
+        assertThrows(Exception.class,
+                () -> {
+                    for (int v : new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+                        b.add(v);
+                    }
+                });
+    }
+
+    @Test
+    void handlesInterspersedRemoves() {
+        int[] insertionLot1 = new int[]{1, 2, 3, 4};
+        int[] insertionLot2 = new int[]{9, 10, 11, 12, 13, 14, 15};
+
+        for (int v: insertionLot1) {
+            b.add(v);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(b.remove(), insertionLot1[i]);
+        }
+
+        for (int v: insertionLot2) {
+            b.add(v);
+        }
+
+    }
+
+    @Test
+    @Tag("hidden")
+    void andSomeStressTesting() {
+        int numLoops = 10;
+        var randomSource = new Random();
+        for (int counter = 0; counter < numLoops; counter++) {
+            int removalsCount = 2; randomSource.nextInt(128);
+            int capacity = removalsCount * 3;
+            int halfRun = capacity / 2;
+            Queue<Integer> q = new Queue<>(capacity);
+            for (int i = 0; i < halfRun; i++) {
+                q.add(i);
+            }
+            for (int i = 0; i < removalsCount; i++) {
+                assertEquals(i, q.remove());
+            }
+            for (int i = halfRun; i < capacity + removalsCount; i++) {
+                q.add(i);
+            }
+            for (int i = removalsCount; i < capacity + removalsCount; i++) {
+                int a = q.remove();
+                assertEquals(i, a);
+            }
+            assertThrows(Exception.class, q::remove);
+        }
+    }
+}
