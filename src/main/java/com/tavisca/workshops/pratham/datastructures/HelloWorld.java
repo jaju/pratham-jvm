0d52b100e570d93ef9b104ca2a83871b078c4864
tavisca-apriyadarshi <51911375+tavisca-apriyadarshi@users.noreply.github.com>
2019-07-23 23:20:43 +0530

    23/7/2019 11:20pm

--- src/main/java/com/tavisca/workshops/pratham/datastructures/HelloWorld.java
+++ src/main/java/com/tavisca/workshops/pratham/datastructures/HelloWorld.java
@@ -1,7 +1,51 @@
 package com.tavisca.workshops.pratham.datastructures;
+//import java.util.Scanner;
 
 public class HelloWorld {
+    String name = "";
+    char gender ;
+    int age;
+    String country;
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //Scanner sc = new Scanner(System.in);
+        //String name = sc.next();
+
+        HelloWorld obj1 = new HelloWorld();
+        obj1.setName("Akshay",'m',21,"India");
+
+        if(obj1.getCountry().equals("Spain")){
+            System.out.println("Hola "+ obj1.getName());
+        }else
+            System.out.println("Hello "+ obj1.getName());
     }
-}
+
+    public void setName(String name, char gender, int age, String country){
+        this.name =  name;
+        this.gender = gender;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getName(){
+        if(age > 0) {
+            if (gender == 'f') {
+                return "Miss " + name;
+            } else if (gender == 'm') {
+                if (age < 18) {
+                    return "Master " + name;
+                } else if (age >= 18)
+                    return "Mr " + name;
+            } else {
+                return "gender not specified";
+            }
+        }else
+            return "incorrect age";
+        return "";
+        }
+
+        public String getCountry(){
+            return country;
+        }
+    }
+
+
---
+++ src/main/java/com/tavisca/workshops/prathm/Rover.java
@@ -0,0 +1,41 @@
+package com.tavisca.workshops.prathm;
+
+public class Rover {
+    private Vector position;
+    //private int x;
+    //private int y;
+    //private char direction;
+
+    public Rover(int x, int y, char direction) {
+        //this.x = x;
+        //this.y = y;
+        //this.direction = direction;
+        position = new Vector(x, y, direction);
+    }
+    public Rover(Vector position){
+        this.position = position;
+    }
+
+
+    public Rover MoveForward() {
+        switch (position.direction){
+            case 'E':
+                position = new Vector(position.x + 1, position.y, position.direction);
+                break;
+            case 'W':
+                position = new Vector(position.x - 1, position.y, position.direction);
+                break;
+            case 'N':
+                position = new Vector(position.x, position.y + 1, position.direction);
+                break;
+            case 'S':
+                position = new Vector(position.x, position.y - 1, position.direction);
+                break;
+        }
+        return new Rover(this.position);
+    }
+
+    public int getPositionOfX() {
+        return position.x;
+    }
+}
